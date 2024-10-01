69fd41b660d0f28f67d8602cd355d988ced40bca
Shani <sh_hatami84@yahoo.com>
2024-10-01 22:12:01 +0330

    Update detect-VBeta11.3_lite.py

--- detect-VBeta11.3_lite.py
+++ detect-VBeta11.3_lite.py
@@ -1,6 +1,6 @@
 import os, numpy as np, cv2, psutil, GPUtil, math, csv, time, re, random, subprocess, platform, logging, threading, multiprocessing
 
-os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["OMP_NUM_THREADS"] = "1" 
 os.environ["OPENBLAS_NUM_THREADS"] = "1"
 os.environ["MKL_NUM_THREADS"] = "1"
 os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
